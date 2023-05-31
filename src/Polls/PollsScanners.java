6c4651f20588932abf85b7177068a289191b96e5
lee000403 <dong000403@gmail.com>
2023-05-31 18:55:22 +0900

    수정

--- src/Polls/PollsScanners.java
+++ src/Polls/PollsScanners.java
@@ -1,5 +1,11 @@
 package Polls;
+import java.util.Scanner;
 
 public class PollsScanners {
-    
+    public String PollsScanners() {
+        String input; 
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        return input;
+    }
 }
---
+++ src/PollsWithMethod.java
@@ -0,0 +1,13 @@
+import Polls.PollsScanners;
+import Polls.PoollInitailArrays;
+
+
+public class PollsWithMethod {
+    public static void main(String[] args) {
+        PoollInitailArrays poollInitailArrays = new PoollInitailArrays();
+        PollsScanners pollsScanners = new PollsScanners();
+        for (int first= 0; first<po)
+        
+        
+    }
+}
