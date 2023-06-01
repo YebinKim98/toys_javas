c7a2e4693f53097396a027bfd93960bfbc724984
lee000403 <dong000403@gmail.com>
2023-06-01 11:18:57 +0900

    스캐너 새로운 함수

---
+++ src/Polls/PollScan.java
@@ -0,0 +1,11 @@
+package Polls;
+
+import java.util.Scanner;
+
+public class PollScan {
+    public int scanner() {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        return num;
+    } 
+}
